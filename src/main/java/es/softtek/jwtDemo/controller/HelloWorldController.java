930a4fec4024e77db78c398f2d3bd3e584dc6160
angelmlnova <amartinez@novasolutionsystems.com>
2019-07-28 11:51:52 -0500

    Se actualiza version basica

--- src/main/java/es/softtek/jwtDemo/controller/HelloWorldController.java
+++ src/main/java/es/softtek/jwtDemo/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package es.softtek.jwtDemo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 	@RequestMapping("hello")
-	public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
-		return "Hello "+name+"!!";
+	public ResponseEntity<Object> helloWorld(@RequestHeader(value="authorization", required = true) String authorization, @RequestParam(value="name", defaultValue="World") String name) {
+		
+		return new ResponseEntity<Object>("Hello "+name+"!!", HttpStatus.OK);
 	}
 }
